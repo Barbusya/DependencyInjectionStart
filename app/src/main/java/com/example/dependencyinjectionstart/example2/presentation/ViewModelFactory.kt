909d174a8e1757490d45760cfcdd3b6524702843
Barbusya <bbbrrr8877@gmail.com>
2022-09-20 12:19:34 +0300

    Subcomponent of Activities

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/ViewModelFactory.kt
@@ -2,16 +2,14 @@ package com.example.dependencyinjectionstart.example2.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.dependencyinjectionstart.example2.di.MyApplicationScope
 import javax.inject.Inject
 import javax.inject.Provider
 
-@MyApplicationScope
 class ViewModelFactory @Inject constructor(
     private val viewModelProviders: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
-): ViewModelProvider.Factory {
+) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-       return viewModelProviders[modelClass]?.get() as T
+        return viewModelProviders[modelClass]?.get() as T
     }
 }

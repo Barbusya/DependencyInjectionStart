c27bfd2c7fb536d029a3944bf744d59f76702135
Barbusya <bbbrrr8877@gmail.com>
2022-09-20 11:39:22 +0300

    Use Provider<ViewModel> instead ViewModel in Factory

--- app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/di/ApplicationComponent.kt
@@ -1,19 +1,17 @@
 package com.example.dependencyinjectionstart.example2.di
 
 import android.content.Context
-import com.example.dependencyinjectionstart.example2.data.database.ExampleDatabase
-import com.example.dependencyinjectionstart.example2.data.network.ExampleApiService
-import com.example.dependencyinjectionstart.example2.presentation.ExampleViewModel
 import com.example.dependencyinjectionstart.example2.presentation.MainActivity
+import com.example.dependencyinjectionstart.example2.presentation.MainActivity2
 import dagger.BindsInstance
 import dagger.Component
-import javax.inject.Singleton
 
 @MyApplicationScope
 @Component (modules = [DataModule::class, DomainModule::class, ViewModelModule::class])
 interface ApplicationComponent {
 
     fun inject(activity: MainActivity)
+    fun inject(activity: MainActivity2)
 
 
     @Component.Factory
